02a812aa063fdfd25408e9bb892b0234a81ab3fb
Mike Ringrose <mikeringrose@GooseCreek.local>
2011-03-21 20:19:11 -0400

    Initial commit of texture wrapper.

--- src/texture.js
+++ src/texture.js
@@ -1,41 +1,66 @@
-var texture;
+// 
+//  texture.js
+//  webgl
+//  
+//  Created by Mike Ringrose on 2011-03-21.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+// 
 
-if (!texture)
-	texture = {};
+// =====================
+// = Texture namespace =
+// =====================
+// 
+//  texture.js
+//  webgl
+//  
+//  Created by Mike Ringrose on 2011-03-21.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+// 
 
-texture.Texture = function() {
+// =====================
+// = Texture namespace =
+// =====================
+var texture = texture || {};
+
+/**
+ * Defines basic functions for handling loaded textures.
+ */
+(function() {
 	
 	var _NEAREST = function(texture) {
-		gl.bindTexture(gl.TEXTURE_2D, texture.getTextureId());
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.getImage());
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);		
-	};
+			gl.bindTexture(gl.TEXTURE_2D, texture.getTextureId());
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.getImage());
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);		
+		},
 	
-	var _LINEAR = function(texture) {
-		gl.bindTexture(gl.TEXTURE_2D, texture.getTextureId());
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.getImage());
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);	
-	};	
+		_LINEAR = function(texture) {
+			gl.bindTexture(gl.TEXTURE_2D, texture.getTextureId());
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.getImage());
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);	
+		},
 	
-	var _MIPMAP = function(texture) {
-		gl.bindTexture(gl.TEXTURE_2D, texture.getTextureId());
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.getImage());
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-		gl.generateMipmap(gl.TEXTURE_2D);	
-	};
-	
+		_MIPMAP = function(texture) {
+			gl.bindTexture(gl.TEXTURE_2D, texture.getTextureId());
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.getImage());
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+			gl.generateMipmap(gl.TEXTURE_2D);	
+		};
+		
 	/**
-	 *  Export our callbacks.
-	 */
+	*  Export our callbacks.
+	*/
 	texture.NEAREST = _NEAREST;
 	texture.LINEAR = _LINEAR;
-	texture.MIPMAP = _MIPMAP;	
-	
+	texture.MIPMAP = _MIPMAP;		
+})();
+
+texture.Texture = function() {	
 	/**
-	 * 
+	 * TODO: kind of a lazy implementation, like the rest of these tools.
+	 * @param config {gl:WebGL object, cb:onload callbck}
 	 */
 	function Texture(config) {
 		var self = this,
@@ -45,7 +70,7 @@ texture.Texture = function() {
 		this._textureId = gl.createTexture();
 		
 		this._image.onload = function() {
-			//- flip our image
+			//- flip our image (prolly should be in the callback, not all images types need flipped?)
 			gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 			
 			//- configure our image
@@ -56,14 +81,14 @@ texture.Texture = function() {
 	}
 	
 	/**
-	 *
+	 * Returns the texture id.
 	 */
 	Texture.prototype.getTextureId = function() {
 		return this._textureId;
 	};
 	
 	/**
-	 *
+	 * Returns the image.
 	 */
 	Texture.prototype.getImage = function() {
 		return this._image;
